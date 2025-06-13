73d82c976697b16e79cc1d45fb2c4ef4b81f866e
isarikaya <isarikayaa@outlook.com>
2025-06-13 16:02:14 +0300

    Refactor: spinner

--- components/ui/spinner.jsx
+++ components/ui/spinner.jsx
@@ -1,7 +1,5 @@
 import { LoaderCircle } from "lucide-react"
 
-const Spinner = () => {
+export const Spinner = () => {
   return <LoaderCircle className="animate-spin size-4 [animation-duration:0.4s]" />
 }
-
-export default Spinner
